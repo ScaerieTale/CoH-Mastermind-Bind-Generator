cfc273b7d0918381526c844bb3e621ab7395165f
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-03-27 20:33:19 -0500

    F*** VS Code.  May it burn in Hell

--- MMBindGenerator.py
+++ MMBindGenerator.py
@@ -1,33 +1,25 @@
-# This script will open a command line interface (CLI) and generate a .txt file
-# titled the user's choice and then insert the pet summoning power name into
-# the appropriate pet command and save the new file within whatever directory the
-# script was run from and will continue looping until you type 'q' to quit.
-
 while True:
-    title = input("Name your .txt file (i.e. Jounin, Commando, etc - 'q' to quit): ")
-    minName = input("What is the pet power name? (eg Call Thugs)  ")
-    if title == 'q':
-        break
-    else: 
-    #creates a function to open, edit, and close a pet txt file
-        f = open(f"{title}.txt", 'w') # 'w' = mode, i.e. 'write'
-        f.write(f"""numpad4 petcom_pow {minName} aggressive
-numpad5 petcom_pow {minName} defensive
-numpad6 petcom_pow {minName} passive
-numpad7 petcom_pow {minName} attack
-numpad8 petcom_pow {minName} follow
-numpad9 petcom_pow {minName} goto
-lctrl+decimal petcom_pow {minName} dismiss""")
-        f.close()
-while True:
-    title2 = input("Enter your LT (second pet's eg Enforcer, GraveKnight, ProtBot) common name ")
-    ltName = input("Enter your LT pet's Power name (e.g. Call Jounin)")
-    f = open(f"{title2}.txt", 'w')
-    f.write(f"""numpad4 petcom_pow {ltName} aggressive
-numpad5 petcom_pow {ltName} defensive
-numpad6 petcom_pow {ltName} passive
-numpad7 petcom_pow {ltName} attack
-numpad8 petcom_pow {ltName} follow
-numpad9 petcom_pow {ltName} goto
-lctrl+decimal petcom_pow {ltName} dismiss""")
-    f.close
+    masterFile = input("Enter the nickname for your Primary (eg Ninjas, Necro, etc): ")
+    tier1 = input("Enter the name or nickname for your Tier 1 pet (eg Genin, Zombie) ")
+    tier2 = input("Next, the name of your tier 2 pet: ")
+    tier3 = input("And finally your tier 3: ")
+    print(f"""Okay, we're almost done!  Next we'll set up your Summon powers to bind them to: 
+        Numpad / ({tier1}, Numpad * ({tier2}), Numpad - ({tier3}) and your Upgrade powers.  Ready?""")
+        t1Sum = input("I need the EXACT name of your tier 1 summoning power (e.g. Call Thugs, Summon Demonling): ")
+        t2Sum = input("Next, your tier 2 (e.g. Call Enforcers): ")
+        t3Sum = input("Finally, your t3 (e.g. Call Bruiser): ")
+        print(f"So that's {t1sum}, {t2sum} and {t3sum}, got it!")
+        print(" Computing...  Okay, last two and your Master file will be complete!")
+        buff1 = input("What is the name of your FIRST primary upgrade power (e.g. Equip Thugs): ")
+        buff2 = input("Now, what is your SECOND upgrade power? (e.g. Upgrade Equipment): ")
+        # create a function to generate and write to a Master file and children
+        f = open(f"{masterFile}MM.txt", 'w') # w = mode, i.e. 'Write'
+        f.write(f"""numpad0 bindloadfilesilent "\all.txt"
+numpad1 bindloadfilesilent "\{tier1}.txt"
+numpad2 bindloadfilesilent "\{tier2}"
+numpad3 bindloadfilesilent "\{tier3}.txt"
+divide powexec_name "summon wolves"
+multiply powexec_name "summon lions"
+subtract powexec_name "summon dire wolf"
+add powexec_name train beasts
+numpadenter powexec_name Tame Beasts""")
