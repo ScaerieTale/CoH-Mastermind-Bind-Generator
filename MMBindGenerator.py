1cd3e924761dc74d6f5b5408a58da047ff09482f
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-03-27 21:24:45 -0500

    Hit a snag, need to look up some things
    I want to put a complicated iterative for each loop at the presently commented section of code.  But I need to look up how to do that in Python so that it changes the variable on each loop.  This was the whole reason for wanting to do this script, so, stay tuned! <3
    - Scaerie

--- MMBindGenerator.py
+++ MMBindGenerator.py
@@ -1,25 +1,35 @@
-while True:
-    masterFile = input("Enter the nickname for your Primary (eg Ninjas, Necro, etc): ")
-    tier1 = input("Enter the name or nickname for your Tier 1 pet (eg Genin, Zombie) ")
-    tier2 = input("Next, the name of your tier 2 pet: ")
-    tier3 = input("And finally your tier 3: ")
-    print(f"""Okay, we're almost done!  Next we'll set up your Summon powers to bind them to: 
-        Numpad / ({tier1}, Numpad * ({tier2}), Numpad - ({tier3}) and your Upgrade powers.  Ready?""")
-        t1Sum = input("I need the EXACT name of your tier 1 summoning power (e.g. Call Thugs, Summon Demonling): ")
-        t2Sum = input("Next, your tier 2 (e.g. Call Enforcers): ")
-        t3Sum = input("Finally, your t3 (e.g. Call Bruiser): ")
-        print(f"So that's {t1sum}, {t2sum} and {t3sum}, got it!")
-        print(" Computing...  Okay, last two and your Master file will be complete!")
-        buff1 = input("What is the name of your FIRST primary upgrade power (e.g. Equip Thugs): ")
-        buff2 = input("Now, what is your SECOND upgrade power? (e.g. Upgrade Equipment): ")
-        # create a function to generate and write to a Master file and children
-        f = open(f"{masterFile}MM.txt", 'w') # w = mode, i.e. 'Write'
-        f.write(f"""numpad0 bindloadfilesilent "\all.txt"
+masterFile = input("Enter the nickname for your Primary (eg Ninjas, Necro, etc): ")
+tier1 = input("Enter the name or nickname for your Tier 1 pet (eg Genin, Zombie) ")
+tier2 = input("Next, the name of your tier 2 pet: ")
+tier3 = input("And finally your tier 3: ")
+t1Sum = input("I need the EXACT name of your tier 1 summoning power (e.g. Call Thugs, Summon Demonling): ")
+t2Sum = input("Next, your tier 2 (e.g. Call Enforcers): ")
+t3Sum = input("Finally, your t3 (e.g. Call Bruiser): ")
+print(f"So that's {t1Sum}, {t2Sum} and {t3Sum}, got it!")
+buff1 = input("What is the name of your FIRST primary upgrade power (e.g. Equip Thugs): ")
+buff2 = input("Now, what is your SECOND upgrade power? (e.g. Upgrade Equipment): ")
+
+# create a function to generate and write to a Master file and children
+f = open(f"{masterFile}MM.txt", 'w') # w = mode, i.e. 'Write'
+f.write(f"""numpad0 bindloadfilesilent "\all.txt"
 numpad1 bindloadfilesilent "\{tier1}.txt"
 numpad2 bindloadfilesilent "\{tier2}"
 numpad3 bindloadfilesilent "\{tier3}.txt"
-divide powexec_name "summon wolves"
-multiply powexec_name "summon lions"
-subtract powexec_name "summon dire wolf"
-add powexec_name train beasts
-numpadenter powexec_name Tame Beasts""")
+divide powexec_name "{t1Sum}"
+multiply powexec_name "{t2Sum}"
+subtract powexec_name "summon {t3Sum}"
+add powexec_name {buff1}
+numpadenter powexec_name {buff2}""")
+f.close
+
+print("We're in the home stretch.  Now we need to create your 'child' files, the files")
+print("that tell your dumdums what to do.")
+trueName1 = input("Please enter your Tier 1 minion's name as it appears in the world (e.g. Zombie, Battle Drone, etc): ")
+trueName2 = input("Now enter your Tier 2 pet's name as above (e.g. Grave Knight): ")
+bobFromAccounting = input("Now enter your tier 3 pet's name as before.")
+f = open(f"{tier1}", 'w')
+# There's going to be an iterative loop here that
+# cycles through creating 3 files for the 3 pet types
+# I just need to learn how to create an iterative loop
+# that updates the variable each time it loops around
+# Stay tuned!
