c6deacfd6bd2e6e2a3fd48064d3a4886434b9a17
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-10 23:52:50 -0500

    Made some progress :)
    Sorry, I know it's going slow.  I've been learning a *lot* lately :)  I've put some more work into the experimental branch here.  Just need to put a for loop in to cycle through the pet files, and then put a GUI on top so it's not a pain to use ♥

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-from asyncore import write
-import files_generator
+master_file = input("Enter the nickname for your Primary (eg Ninjas, Necro, etc): ")
+tier_1 = input("Enter the name or nickname for your Tier 1 pet (eg Genin, Zombie) ")
+tier_2 = input("Next, the name of your tier 2 pet: ")
+tier_3 = input("And finally your tier 3: ")
+tier_1_summon = input("I need the EXACT name of your tier 1 summoning power (e.g. Call Thugs, Summon Demonling): ")
+tier_2_summon = input("Next, your tier 2 (e.g. Call Enforcers): ")
+tier_3_summon = input("Finally, your tier 3 (e.g. Call Bruiser): ")
+buff_1 = input("What is the name of your FIRST primary upgrade power (e.g. Equip Thugs): ")
+buff_2 = input("Now, what is your SECOND upgrade power? (e.g. Upgrade Equipment): ")
 
-masterFile = input("Enter the nickname for your Primary (eg Ninjas, Necro, etc): ")
-tier1 = input("Enter the name or nickname for your Tier 1 pet (eg Genin, Zombie) ")
-tier2 = input("Next, the name of your tier 2 pet: ")
-tier3 = input("And finally your tier 3: ")
-t1Sum = input("I need the EXACT name of your tier 1 summoning power (e.g. Call Thugs, Summon Demonling): ")
-t2Sum = input("Next, your tier 2 (e.g. Call Enforcers): ")
-t3Sum = input("Finally, your t3 (e.g. Call Bruiser): ")
-print(f"So that's {t1Sum}, {t2Sum} and {t3Sum}, got it!")
-buff1 = input("What is the name of your FIRST primary upgrade power (e.g. Equip Thugs): ")
-buff2 = input("Now, what is your SECOND upgrade power? (e.g. Upgrade Equipment): ")
-
-# create a function to generate and write to a Master file and children
-f = open(f"{masterFile}MM.txt", 'w') # w = mode, i.e. 'Write'
-f.write(f"""numpad0 bindloadfilesilent "\all.txt"
-numpad1 bindloadfilesilent "\{tier1}.txt"
-numpad2 bindloadfilesilent "\{tier2}"
-numpad3 bindloadfilesilent "\{tier3}.txt"
-divide powexec_name "{t1Sum}"
-multiply powexec_name "{t2Sum}"
-subtract powexec_name "summon {t3Sum}"
-add powexec_name {buff1}
-numpadenter powexec_name {buff2}""")
-f.close
-
-print("We're in the home stretch.  Now we need to create your 'child' files, the files")
-print("that tell your dumdums what to do.")
-trueName1 = input("Please enter your Tier 1 minion's name as it appears in the world (e.g. Zombie, Battle Drone, etc): ")
-trueName2 = input("Now enter your Tier 2 pet's name as above (e.g. Grave Knight): ")
-bobFromAccounting = input("Now enter your tier 3 pet's name as before.")
+f = open(f"{master_file}MM.txt", 'w')
+f.write(f'''numpad0 bindloadfilesilent "\all.txt"
+numpad1 bindloadfilesilent "\{tier_1}.txt"
+numpad2 bindloadfilesilent "\{tier_2}.txt"
+numpad3 bindloadfilesilent "\{tier_3}.txt"
+divide powexec_name "{tier_1_summon}"
+multiply powexec_name "{tier_2_summon}"
+subtract powexec_name "{tier_3_summon}"
+add powexec_name {buff_1}
+numpadenter powexec_name {buff_2}
+''')
+# f = open(f"{title}.txt", 'w')
+# f.write(f"""numpad4 petcom_pow {minName} aggressive
+# numpad5 petcom_pow {minName} defensive
+# numpad6 petcom_pow {minName} passive
+# numpad7 petcom_pow {minName} attack
+# numpad8 petcom_pow {minName} follow
+# numpad9 petcom_pow {minName} goto
+# lctrl+decimal petcom_pow {minName} dismiss""")
+#         f.close()
+#
