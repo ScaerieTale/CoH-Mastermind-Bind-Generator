48529e8a0c51cfffc181bde7ddaf503bbb6ebf39
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-26 15:29:54 -0500

    Scaffolding in place
    I've got the scaffolding in place for the GUI.  Now I just need to hook everything up :)

--- main.py
+++ main.py
@@ -1,56 +1,113 @@
+# GUI framework
+from tkinter import *
+
+# main window setup
+root = Tk()
+root.maxsize(1600,900)
+root.title("ScaerieTale's MM Bind Generator")
+
+# functions (More to be added later)
+def run_clicked():
+    pass
+
+# labels
+Label()
+master_label = Label(justify="right", text="""Name your Main
+MM.txt file.  This can be anything you want.""").grid(row=0, column=0)
+tier_1_label = Label(justify="right",text="Enter Tier 1 pet nickname:").grid(row=1, column=0)
+tier_2_label = Label(justify="right",text="Enter Tier 2 pet nickname:").grid(row=2, column=0)
+tier_3_label = Label(justify="right",text="Enter Tier 3 pet nickname:").grid(row=3, column=0)
+
+summon_explanation = Label(justify="right",text="""For the next three fields
+please enter your pets' names EXACTLY as they
+appear in game:""").grid(row=4, column=0)
+
+tier_1_summon_label = Label(justify="right",text="Summoned tier 1:").grid(row=5, column=0)
+tier_2_summon_label = Label(justify="right",text="Summoned tier 2:").grid(row=6, column=0)
+tier_3_summon_label = Label(justify="right",text="Summoned tier 3:").grid(row=7, column=0)
+
+buffs_label = Label(justify="right",text="""Enter the two CLASS PRIMARY
+buffs below (NOT any secondary buffs)""").grid(row=8, column=0)
+buff_1_label = Label(justify="right",text="Buff 1 (level 6):").grid(row=9, column=0)
+buff_2_label = Label(justify="right",text="Buff 2 (level 32):").grid(row=10, column=0)
+
+
+# Functions (fields, button, etc.)
+entry_master = Entry(justify="left",).grid(row=0, column=1)
+entry_t1 = Entry(justify="left",).grid(row=1, column=1)
+entry_t2 = Entry(justify="left",).grid(row=2, column=1)
+entry_t3 = Entry(justify="left",).grid(row=3, column=1)
+
+spacer_1 = Label(text="          ").grid(row=4, column=1)
+
+entry_t1_summon = Entry(justify="left",).grid(row=5, column=1)
+entry_t2_summon = Entry(justify="left",).grid(row=6, column=1)
+entry_t3_summon = Entry(justify="left",).grid(row=7, column=1)
+
+spacer_2 = Label(text="          ").grid(row=8, column=1)
+
+
+entry_buff_1 = Entry(justify="left",).grid(row=9, column=1)
+entry_buff_2 = Entry(justify="left",).grid(row=10, column=1)
+run_button = Button(justify="left",text="Run").grid(row=11, column=1)
+
+
+root.mainloop()
+
+
 # 20 questions!  Will replace this with a graphical interface ASAP
-master_file = input("Enter the nickname for your Primary (eg Ninjas, Necro, etc): ")
-tier_1 = input("Enter the name or nickname for your Tier 1 pet (eg Genin, Zombie) ")
-tier_2 = input("Next, the name of your tier 2 pet: ")
-tier_3 = input("And finally your tier 3: ")
-tier_1_summon = input("I need the EXACT name of your tier 1 summoning power (e.g. Call Thugs, Summon Demonling): ")
-tier_2_summon = input("Next, your tier 2 (e.g. Call Enforcers): ")
-tier_3_summon = input("Finally, your tier 3 (e.g. Call Bruiser): ")
-buff_1 = input("What is the name of your FIRST primary upgrade power (e.g. Equip Thugs): ")
-buff_2 = input("Now, what is your SECOND upgrade power? (e.g. Upgrade Equipment): ")
+# master_file = input("Enter the nickname for your Primary (eg Ninjas, Necro, etc): ")
+# tier_1 = input("Enter the name or nickname for your Tier 1 pet (eg Genin, Zombie) ")
+# tier_2 = input("Next, the name of your tier 2 pet: ")
+# tier_3 = input("And finally your tier 3: ")
+# tier_1_summon = input("I need the EXACT name of your tier 1 summoning power (e.g. Call Thugs, Summon Demonling): ")
+# tier_2_summon = input("Next, your tier 2 (e.g. Call Enforcers): ")
+# tier_3_summon = input("Finally, your tier 3 (e.g. Call Bruiser): ")
+# buff_1 = input("What is the name of your FIRST primary upgrade power (e.g. Equip Thugs): ")
+# buff_2 = input("Now, what is your SECOND upgrade power? (e.g. Upgrade Equipment): ")
 
 # "Master" file generator as a *MM.txt file :)
-file = open(f"{master_file}MM.txt", 'w')
-file.write(f'''numpad0 bindloadfilesilent "\all.txt"
-numpad1 bindloadfilesilent "\{tier_1}.txt"
-numpad2 bindloadfilesilent "\{tier_2}.txt"
-numpad3 bindloadfilesilent "\{tier_3}.txt"
-divide powexec_name "{tier_1_summon}"
-multiply powexec_name "{tier_2_summon}"
-subtract powexec_name "{tier_3_summon}"
-add powexec_name {buff_1}
-numpadenter powexec_name {buff_2}
-''')
-file.close()
+# file = open(f"{master_file}MM.txt", 'w')
+# file.write(f'''numpad0 bindloadfilesilent "\all.txt"
+# numpad1 bindloadfilesilent "\{tier_1}.txt"
+# numpad2 bindloadfilesilent "\{tier_2}.txt"
+# numpad3 bindloadfilesilent "\{tier_3}.txt"
+# divide powexec_name "{tier_1_summon}"
+# multiply powexec_name "{tier_2_summon}"
+# subtract powexec_name "{tier_3_summon}"
+# add powexec_name {buff_1}
+# numpadenter powexec_name {buff_2}
+# ''')
+# file.close()
 
-# So, I don't fully understand WHY this works, but I'm not
-# going to complain either.  Basically setting up the pets
-# as a dictionary like this, lets me call both pet nickname
-# (for the file name) and pet summon name (fr ingame use) within
-# a single for loop.
-pets = {
-    tier_1: tier_1_summon,
-    tier_2: tier_2_summon,
-    tier_3: tier_3_summon
-}
-# "child" pet files generator
-for pet, summon in pets.items():
-    file = open(f"{pet}.txt", 'w')
-    file.write(f"""numpad4 petcom_pow {summon} aggressive
-numpad5 petcom_pow {summon} defensive
-numpad6 petcom_pow {summon} passive
-numpad7 petcom_pow {summon} attack
-numpad8 petcom_pow {summon} follow
-numpad9 petcom_pow {summon} goto
-lctrl+decimal petcom_pow {summon} dismiss""")
-    file.close()
+# # So, I don't fully understand WHY this works, but I'm not
+# # going to complain either.  Basically setting up the pets
+# # as a dictionary like this, lets me call both pet nickname
+# # (for the file name) and pet summon name (fr ingame use) within
+# # a single for loop.
+# pets = {
+#     tier_1: tier_1_summon,
+#     tier_2: tier_2_summon,
+#     tier_3: tier_3_summon
+# }
+# # "child" pet files generator
+# for pet, summon in pets.items():
+#     file = open(f"{pet}.txt", 'w')
+#     file.write(f"""numpad4 petcom_pow {summon} aggressive
+# numpad5 petcom_pow {summon} defensive
+# numpad6 petcom_pow {summon} passive
+# numpad7 petcom_pow {summon} attack
+# numpad8 petcom_pow {summon} follow
+# numpad9 petcom_pow {summon} goto
+# lctrl+decimal petcom_pow {summon} dismiss""")
+#     file.close()
 
-file = open("all.txt", 'w')
-file.write("""numpad4 petcom_all aggressive
-numpad5 petcom_all defensive
-numpad6 petcom_all passive
-numpad7 petcom_all attack
-numpad8 petcom_all follow
-numpad9 petcom_all goto
-lctrl+decimal petcom_all dismiss""")
-file.close()
+# file = open("all.txt", 'w')
+# file.write("""numpad4 petcom_all aggressive
+# numpad5 petcom_all defensive
+# numpad6 petcom_all passive
+# numpad7 petcom_all attack
+# numpad8 petcom_all follow
+# numpad9 petcom_all goto
+# lctrl+decimal petcom_all dismiss""")
+# file.close()
