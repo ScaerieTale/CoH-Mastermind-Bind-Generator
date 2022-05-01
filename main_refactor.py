de272ef733e8f1aa614102740c8b726df33f97e0
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-05-01 06:50:26 -0500

    Added TODOs outlining what's left to add Changed Dark/Light theme to be Hero/Villain themes instead! Added new hero/villain theme buttons!

--- main_refactor.py
+++ main_refactor.py
@@ -1,8 +1,7 @@
 # Constants
-BG_DARK_MODE = "#6c1e00"
-FG_DARK_MODE = "#a58ec3"
-BG_LIGHT_MODE = "#7fafff"
-FG_LIGHT_MODE = "#6e50af"
+BG_VILLAIN_MODE = "#6c1e00"
+BG_HERO_MODE = "#6e50af"
+FG_COLOR = "#D5BEDF"
 FONT_FAMILY = "arial, sans"
 SMALL_TEXT = 12
 MEDIUM_TEXT = 18
@@ -11,46 +10,55 @@ LARGE_TEXT = 28
 # Defaults
 font_size = MEDIUM_TEXT
 font_setup = (FONT_FAMILY, font_size)
-dark_mode = True
+villain_mode = True
 
-# Set up Dark/Light Mode switch functionality
-if dark_mode is True:
-    background_color = BG_DARK_MODE
-    foreground_color = FG_DARK_MODE
+# Set up Hero/Villain Mode switch functionality
+if villain_mode is True:
+    background_color = BG_VILLAIN_MODE
 else:
-    background_color = BG_LIGHT_MODE
-    foreground_color = FG_LIGHT_MODE
+    background_color = BG_HERO_MODE
  
 
 # GUI framework
-from statistics import median
 from tkinter import *
-from turtle import back
 
-# main window setup
+# main window
 root = Tk()
 root.maxsize(1600,900)
 root.title("ScaerieTale's Mastermind Bind Generator")
-
-# Functions
-
-
+root.config(bg=background_color)
+root.minsize(300, 300)
 
 
-# Labels
-test_label = Label(text="Testing", font=font_setup, bg=background_color, fg=foreground_color)
-test_label.pack()
+# TODO: Hero/Villain Mode Switch
+# This will consist of two separate buttons, one red and one blue.
+# When in hero mode (blue BG), a red Villain Mode button will display
+# Vice Versa in Villain mode.  The "Run" button will also be themed
+# And, I want to add a themed File Browser button/field
 
-
-
-
-
+# TODO Functions
+# So the buttons do things when clicked
 
 
 
+# TODO: Labels
 
 
 
+
+# TODO Entry fields
+# Including a new File Browser entry
+
+
+
+# TODO Buttons (See repo to see what they'll look like)
+# Run button, File Browser button (to select where to save the files)
+# Hero/Villain Mode button (To change the color scheme)
+
+
+
+
+# TODO Migrate the "guts" of the code so it actually functions
 
 
 
